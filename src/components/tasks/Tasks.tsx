9b2fcf7c61316727adcb2f5519936b6ce16feecf
steniomoreira <steniomoreiradev@gmail.com>
2023-03-02 02:08:26 -0300

    feat: added task content in todo list

--- src/components/tasks/Tasks.tsx
+++ src/components/tasks/Tasks.tsx
@@ -1,6 +1,7 @@
 import { Empty } from '../empty/Empty';
 import { Info } from '../info/Info';
 import styles from './Tasks.module.css';
+import {Trash} from 'phosphor-react';
 
 const counter = 10;
 const done = 6;
@@ -11,6 +12,19 @@ export function Tasks() {
       <div className={styles.info}>
         <Info text='Tarefas criadas' counter={counter} />
         <Info text='Concluídas' counter={counter} done={done}/>
+      </div>
+
+      <div className={styles.list}>
+        <div className={styles.taskContent}>
+          <input id='tempID' type="checkbox" />
+          <label htmlFor='tempID'  />
+
+          <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+          
+          <button type='button'>
+            <Trash size={24} />
+          </button>
+        </div>
       </div>
 
       <Empty />
