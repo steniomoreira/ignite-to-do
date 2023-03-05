ba916d832a03702d6ef0eb7a3008ad39ae8d7fca
steniomoreira <steniomoreiradev@gmail.com>
2023-03-05 17:07:59 -0300

    faet: added message error in new task input

--- src/components/newTask/NewTask.tsx
+++ src/components/newTask/NewTask.tsx
@@ -1,5 +1,5 @@
-import { PlusCircle } from "phosphor-react";
-import { ChangeEvent, FormEvent } from "react";
+import { PlusCircle, WarningCircle } from "phosphor-react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { Button } from "../button/Button";
 import { Input } from "../input/Input";
 
@@ -12,10 +12,32 @@ interface NewTaskProps {
 }
 
 export function NewTask({newTask, handleCreateNewTask, handleOnChangeTask}: NewTaskProps) {
+  const [error, setError] = useState(false);
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    if (!newTask) {      
+      return setError(true);
+    }
+
+    handleCreateNewTask(event)
+  }
+
+  useEffect(() => {
+    newTask && setError(false);
+  },[newTask])
 
   return (
-    <form className={styles.newTask} onSubmit={handleCreateNewTask}>
-     <Input value={newTask} placeholder="Adicione uma nova tarefa" onChange={handleOnChangeTask} required/>
+    <form className={styles.newTask} onSubmit={handleSubmit}>
+      {error && (
+        <div className={styles.error}>
+          <WarningCircle size={16} />
+          <p>A tarefa não pode ser criada sem descrição!</p>
+        </div>
+      )}
+
+      <Input value={newTask} placeholder="Adicione uma nova tarefa" onChange={handleOnChangeTask}/>
 
       <Button text="Criar" type="submit" >
         <PlusCircle size={16} />
